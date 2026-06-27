d70489b1151f1c35dd56a18b858259616c12df05
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 10:27:11 -0500

    Solucion pregunta 4

--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_04():
     """
@@ -20,3 +20,8 @@ def pregunta_04():
     E    4.785714
     Name: c2, dtype: float64
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+
+    promedio = df.groupby("c1")["c2"].mean().sort_index()
+
+    return promedio
