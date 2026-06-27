24548a535c01ad29f3ec4d059b0ce3898c86de46
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 12:07:10 -0500

    Solucion pregunta 10

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_10():
     """
@@ -20,3 +20,8 @@ def pregunta_10():
     D                   1:2:3:5:5:7
     E   1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    
+    tabla = (df.groupby("c1")["c2"].apply(lambda x: ":".join(map(str, sorted(x)))).to_frame())
+
+    return tabla
