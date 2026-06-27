8ffa2bd3590104f440181e5e8266b8d1ba1d590a
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 11:33:23 -0500

    Solucion pregunta 9

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_09():
     """
@@ -23,3 +23,7 @@ def pregunta_09():
     39  39  E   5  1998-01-26  1998
 
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df["year"] = df["c3"].str[0:4]
+
+    return df
