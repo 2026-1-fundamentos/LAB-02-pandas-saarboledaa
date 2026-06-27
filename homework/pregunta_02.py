27e543b3d9a12c99555e7f3c054101c86557f44a
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 10:18:58 -0500

    Solucion pregunta 2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_02():
     """
@@ -14,3 +14,7 @@ def pregunta_02():
     4
 
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    columnas = df.shape[1]
+    
+    return columnas
