a2cbdd7235d380c1e28a6dc21516a767550ee515
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 11:28:40 -0500

    Solucion pregunta 8

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_08():
     """
@@ -22,3 +22,7 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df["suma"] = df["c0"] + df["c2"]
+
+    return df
