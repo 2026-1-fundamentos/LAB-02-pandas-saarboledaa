40a88517fabe672bcab71f4a6ca0a51cc7b7b511
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 10:24:00 -0500

    Solucion pregunta 3

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_03():
     """
@@ -21,3 +21,7 @@ def pregunta_03():
     Name: count, dtype: int64
 
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    datos = df["c1"].value_counts().sort_index()
+
+    return datos
