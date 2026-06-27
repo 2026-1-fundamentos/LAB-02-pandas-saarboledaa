895bbb0b1f572d18c981c319855706b30086f989
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 17:09:38 -0500

    Solucion pregunta 13

--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_13():
     """
@@ -20,3 +20,9 @@ def pregunta_13():
     E    275
     Name: c5b, dtype: int64
     """
+    df0 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+
+    suma = pd.merge(df0, df2, on="c0").groupby("c1")["c5b"].sum().sort_index()
+
+    return suma
