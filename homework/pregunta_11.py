bc7229ab5f52c87a0d65fe8bc9600e833a3562a9
Santiago Arboleda <saarboledaa@unal.edu.co>
2026-06-27 17:00:27 -0500

    Solucion pregunta 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_11():
     """
@@ -22,3 +22,8 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
+    df = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+
+    tabla = (df.groupby("c0")["c4"].apply(lambda x: ",".join(map(str, sorted(x)))).reset_index())
+    
+    return tabla
